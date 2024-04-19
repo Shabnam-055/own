1ea25129534bc23b1c10954f6828c2ec6fcfa7b3
shabnamk <shabnam2gmail.com>
2024-04-19 09:05:11 +0530

    adding shabnam's file

---
+++ 1st pgm.py
@@ -0,0 +1,25 @@
+a=10
+y=110
+z=120
+b=20.0
+c="welcome to data structure with python"
+d=[1,2,3,4,5,]
+e={"name":"shabnam","age":17}
+f=("apple","banana","cherry")
+print("integer",a)
+print("float",b)
+print("string",c)
+print("list",d)
+print("dictionary",e)
+print("tuple",f)
+d.append(7)
+print("list:", d)
+q=len(d)
+print(q)
+w=d.index(2)
+print(w)
+for i in e:
+	print(i)
+print(y>z)
+k=len(c)
+print(k)
